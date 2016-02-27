a98569260455f985da8b9a65b61dc163f6c78fe7
振亮 孙 <solszl@163.com>
2016-02-28 01:15:51 +0800

    + oauthswift lib

--- Shrimp500px/Constants/AppConfig.swift
+++ Shrimp500px/Constants/AppConfig.swift
@@ -10,6 +10,10 @@ import Foundation
 import UIKit
 
 
+let CustomKey = "SHxQG62Gp8UYe8ZTArCXDdH404HFXtPvMlhqth3U"
+
+let CustomSecret = "Pb4xgk7jlSwnl6jE58ePG3eW90GCejmo8Z9yCy3m"
+
 let ScreenWidth = UIScreen.mainScreen().bounds.width
 
 let ScreenHeight = UIScreen.mainScreen().bounds.height
