e681604c44d763e2d8cebad7599a98cf31af907f
振亮 孙 <solszl@163.com>
2016-03-05 22:01:19 +0800

    * for build

---
+++ Shrimp500px/Service/Bizz/Router.swift
@@ -0,0 +1,20 @@
+//
+//  Router.swift
+//  Shrimp500px
+//
+//  Created by 振亮 孙 on 16/3/3.
+//  Copyright © 2016年 papa.studio. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum Router {
+
+    static let customer_key = "7AqOElDfu35aVav8bgztEdMWLVojPEFZoAp66JJw"
+    
+    static let bizzPhoto = BizzPhoto()
+//    var URLRequest: NSMutableURLRequest {
+//        
+//    }
+}
---
+++ Shrimp500px/Service/BusinessService.swift
@@ -0,0 +1,9 @@
+//
+//  BusinessService.swift
+//  Shrimp500px
+//
+//  Created by 振亮 孙 on 16/3/3.
+//  Copyright © 2016年 papa.studio. All rights reserved.
+//
+
+import Foundation
